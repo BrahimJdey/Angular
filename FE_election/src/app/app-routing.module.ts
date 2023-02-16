c41d02e837a3312b68e769063173ebae0b538e7d
bhm <brahimjdey@gmail.com>
2023-02-16 23:41:55 +0000

    angular_frnt

--- FE_election/src/app/app-routing.module.ts
+++ FE_election/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DummyComponent } from './dummy/dummy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ 
+  { path:'etudiant',component: DummyComponent },
+  { path:'candidateur',component: DummyComponent },
+  { path:'centre_vote',component: DummyComponent },
+  { path:'sign up',component: DummyComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FE_election/src/app/app.module.ts
+++ FE_election/src/app/app.module.ts
@@ -2,23 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DummyComponent } from './dummy/dummy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DummyComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
     
   ],
   providers: [],
